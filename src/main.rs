8c5696339a2ec83299dece9bd6301b17d74e511d
Lin Clark <lclark@mozilla.com>
2016-12-30 20:35:35 -0500

    Add support for address lines.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,20 @@ fn main() {
 }
 
 fn translate(assembly: String) -> String {
-    assembly
+    assembly.lines()
+            .map(|x| encode_line(x))
+            .collect()
+}
+
+fn encode_line(line: &str) -> String {
+    let mut l = match line {
+        line if line.contains("@") => {
+            format!("{:016b}", line[1..].parse::<u16>().unwrap())
+        },
+        _ => String::from(line)
+    };
+    l.push_str("\n");
+    l
 }
 
 fn get_assembly() -> String {
