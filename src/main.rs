122b104a8843c949207a228009ec0f12db65a8c0
Lin Clark <lclark@mozilla.com>
2016-12-30 19:37:21 -0500

    Basic file I/O.

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+// open.rs
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::env;
+
+fn main() {
+    let assembly = get_assembly();
+    let machine_code = translate(assembly);
+    write_machine_code(machine_code);
+}
+
+fn translate(assembly: String) -> String {
+    assembly
+}
+
+fn get_assembly() -> String {
+    let mut args = env::args();
+    let filename = args.nth(1).unwrap();
+
+    let path = Path::new(filename.as_str());
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(err) => panic!("couldn't open {}: {}", display, err.description()),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    let assembly = match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display,
+                                                   why.description()),
+        Ok(_) => s,
+    };
+    assembly
+}
+
+fn write_machine_code(machine_code: String) {
+    let mut args = env::args();
+    let filename = args.nth(2).unwrap();
+
+    let path = Path::new(filename.as_str());
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(err) => panic!("couldn't create {}: {}", display, err.description()),
+        Ok(file) => file,
+    };
+
+    match file.write_all(machine_code.as_bytes()) {
+        Err(err) => {
+            panic!("couldn't write to {}: {}", display, err.description())
+        },
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+}
