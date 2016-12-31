72e5970466f2effffa2c77c0d57d2d23fa13c413
Lin Clark <lclark@mozilla.com>
2016-12-31 09:56:44 -0500

    Add test for a commands.

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,20 @@
 pub fn parse(asm: String) -> String {
     asm.lines()
-       .map(|x| encode_line(x))
+       .map(|x| encode_line(x) + "\n")
        .collect()
 }
 
 fn encode_line(line: &str) -> String {
-    let mut l = match line {
+    match line {
         line if line.contains("@") => {
             format!("{:016b}", line[1..].parse::<u16>().unwrap())
         },
         _ => String::from(line)
-    };
-    l.push_str("\n");
-    l
+    }
+}
+
+#[test]
+fn encodes_a_command() {
+    let encoded = encode_line("@8");
+    assert_eq!(encoded, "0000000000001000");
 }
